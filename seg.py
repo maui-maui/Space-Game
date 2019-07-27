52308af21b289373e23ab58ad0d275f06877d2b7
maui-maui <maui-maui@users.noreply.github.com>
2019-07-27 05:21:38 -0500

    Start of new seg.py Starting file

---
+++ seg.py
@@ -0,0 +1,17 @@
+#####################
+### Basic Imports ###
+#####################
+import time
+import os
+import sys
+import json
+import random
+######################
+### Custom Imports ###
+######################
+from data.definitions import *
+from data.classes import *
+from data.commands import * 
+########################
+##### Game Startup #####
+########################
