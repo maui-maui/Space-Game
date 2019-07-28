f1d0dc168f838a395b48e96ec9cb213c96a71a78
Kirari <53339220+Kirari-Kyoko@users.noreply.github.com>
2019-07-29 00:23:57 +0200

    fixup! Quick Fix some Stuff

--- utils/map_tools.py
+++ utils/map_tools.py
@@ -4,6 +4,7 @@ An Util which has Utilitys to generate an Text Based Map
 Contains:
     Generate Sector: An Map Generator which Generates an Weighted Map Grid
     Visualise Sector: Takes an Map Grid and Prints it out in an Human Readable form
+    Generate Quadrant: An Map Generator which generates basicly an "Galaxy" where Sectors are in
 """
 
 import bisect
@@ -25,24 +26,14 @@ def generate_sector(size: int, object_weight: list) -> dict:
         object_weight: An Nested List with Object / Value Types
 
     Examples:
-        generate_sector(6, [["*", 50], ["#", 10]]) would output something like this
-
-        ---------------------
-        |  | 01 02 03 04 05 |
-        ---------------------
-        |01| *  *  *  *  *  |
-        |02| *  *  *  *  #  |
-        |03| *  *  *  *  #  |
-        |04| *  *  *  #  *  |
-        |05| *  #  *  *  #  |
-        ---------------------
+        generate_sector(6, [["*", 50], ["#", 10]]) would output an Map File where * is far more Common than #
 
     Returns:
         An Dict with Lists inside which Represent the Map Data per Row
     """
 
     output = {}
-
+    placed_player = False
     totals = []
     running_total = 0
 
@@ -50,23 +41,36 @@ def generate_sector(size: int, object_weight: list) -> dict:
         running_total += w[1]
         totals.append(running_total)
 
+    def next():
+        """
+        Gets an Random Object from the Object - Weight List
+        """
+        ran = random.random() * totals[-1]
+        i = bisect.bisect_right(totals, ran)
+        return object_weight[i][0]
+
     for x in range(1, size):
         row = []
         for y in range(1, size):
-            ran = random.random() * totals[-1]
-            i = bisect.bisect_right(totals, ran)
-            object = object_weight[i][0]
+            object = next()
+            if placed_player is False and object is "@":
+                row.append(object)
+            elif placed_player is True and object is "@":
+                while object is "@":
+                    object = next()
             row.append(object)
         output[x] = row
     return output
 
 
 def generate_quadrant(size: int) -> dict:
+    """Empty"""
     pass
 
 
 # TODO: Rewrite Print_Sector Code
 def visualise_sector(mapdata: dict):
+    """No Doc"""
     map_size = len(mapdata) + 1
     text = "|  | "
     for index in range(1, map_size):
@@ -104,6 +108,3 @@ def visualise_sector(mapdata: dict):
         print(text)
 
     print(seperator)
-
-
-print_sector(generate_sector(6, [["*", 50], ["#", 10]]))
