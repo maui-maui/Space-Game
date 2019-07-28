1626d0c92cfda0b99fee0d4ff0fbf99ca496a547
Kirari <53339220+Kirari-Kyoko@users.noreply.github.com>
2019-07-28 23:55:08 +0200

    Renamed print_sector() to visualise_sector() to avoid having autocomplete conflicts with Print, Made generate_sector() weighted

--- utils/map_tools.py
+++ utils/map_tools.py
@@ -1,12 +1,12 @@
 """
-Tools for Generating Sectors and Quadrants
+An Util which has Utilitys to generate an Text Based Map
 
 Contains:
-    generate_sector - Generates an Sector
-    generate_quadrant - Generates an Quadrant
-    print_sector - Prints the Sector into the Console
+    Generate Sector: An Map Generator which Generates an Weighted Map Grid
+    Visualise Sector: Takes an Map Grid and Prints it out in an Human Readable form
 """
 
+import bisect
 import random
 
 import colorama
@@ -14,126 +14,59 @@ from colorama import Fore
 
 colorama.init()
 
-# TODO: Add Error Catching when no @ got Generated
-def generate_sector(size: int) -> dict:
+
+# TODO: Make @ Guaranteed to be in there
+def generate_sector(size: int, object_weight: list) -> dict:
     """
-    Generates the Sector Map Data
+    Generates an Sector with Weighted Spawns
 
-    generate_sector(9) would generate an 9x9 Map
+    Args:
+        size: Int Representing the Size of the Sector (Size X Size)
+        object_weight: An Nested List with Object / Value Types
 
-    Dict Structure:
-        {
-            0: [MapData]
-            1: [MapData]
-        \
+    Examples:
+        generate_sector(6, [["*", 50], ["#", 10]]) would output something like this
 
-    Warning: Keep in Mind that the dict and lists are zero indexed meaning 0 is the first value
+        ---------------------
+        |  | 01 02 03 04 05 |
+        ---------------------
+        |01| *  *  *  *  *  |
+        |02| *  *  *  *  #  |
+        |03| *  *  *  *  #  |
+        |04| *  *  *  #  *  |
+        |05| *  #  *  *  #  |
+        ---------------------
 
-    Parameters
-    ----------
-    size: Int Representing how Large the Map Grid should be
-
-    Returns
-    -------
-    An Dict with Lists containing the Lists with the Row Data
+    Returns:
+        An Dict with Lists inside which Represent the Map Data per Row
     """
 
     output = {}
-    player_set = False
-    map_objects = ["#", "P", "C", "!", "^", "B", "@"]
 
-    for x in range(size):
+    totals = []
+    running_total = 0
+
+    for w in object_weight:
+        running_total += w[1]
+        totals.append(running_total)
+
+    for x in range(1, size):
         row = []
-        for y in range(size):
-            random_object = random.choice(map_objects)
-            random_int = random.randint(0, 5)
-
-            # Prevents Multiple Player Locations to be Spawned
-            if random_object is "@" and player_set is True:
-                while random_object is "@":
-                    random_object = random.choice(map_objects)
-            elif random_object is "@" and player_set is False:
-                row.append("@")
-                player_set = True
-                continue
-
-            # Make Commanders less Common
-            if random_object is "C":
-                if random_int <= 2:
-                    row.append("C")
-                    continue
-                else:
-                    row.append("#")
-                    continue
-
-            # Make Enemy Bases less Common
-            if random_object is "!":
-                if random_int is 1:
-                    row.append("!")
-                    continue
-                else:
-                    row.append("#")
-                    continue
-
-            # Make Friendly Bases Less Common
-            if random_object is "B":
-                if random_int <= 2:
-                    row.append("B")
-                    continue
-                else:
-                    row.append("#")
-                    continue
-
-            row.append(random_object)
+        for y in range(1, size):
+            ran = random.random() * totals[-1]
+            i = bisect.bisect_right(totals, ran)
+            object = object_weight[i][0]
+            row.append(object)
         output[x] = row
-
     return output
 
 
 def generate_quadrant(size: int) -> dict:
-    """
-    Generates the Quadrant Map Data
-
-    generate_sector(9) would generate an 9x9 Map
-
-    Dict Structure:
-        {
-            Quadrant1: {Sector Data}
-            Quadrant2: {Sector Data}
-        \
-
-    Parameters
-    ----------
-    size: Int Representing how Large the Map Grid should be
-
-    Returns
-    -------
-    An Dict with Nested Dict containing the Sector Map Data
-    """
     pass
 
 
-def print_sector(mapdata: dict):
-    """
-    Prints the Map Data into the Console
-
-    ---------------------
-    |  | 1  2  3  4  5  |
-    ---------------------
-    | 1| P  B  *  P  C  |
-    | 2| P  ^  P  C  P  |
-    | 3| ^  *  @  *  ^  |
-    | 4| P  *  *  ^  *  |
-    | 5| P  !  P  ^  ^  |
-    ---------------------
-
-    Works with any Map Size
-
-    Parameters
-    ----------
-    mapdata: dict An Dict Containing the Map Data (generate it with generate_sector)
-    """
-
+# TODO: Rewrite Print_Sector Code
+def visualise_sector(mapdata: dict):
     map_size = len(mapdata) + 1
     text = "|  | "
     for index in range(1, map_size):
@@ -171,3 +104,6 @@ def print_sector(mapdata: dict):
         print(text)
 
     print(seperator)
+
+
+print_sector(generate_sector(6, [["*", 50], ["#", 10]]))
